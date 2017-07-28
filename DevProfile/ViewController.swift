8a1255a1d18fe5167992bd01446ab79dccf0ed17
Sebastian C <crossasebastian@gmail.com>
2017-07-27 22:09:32 -0600

    Completed phone autolayout

--- DevProfile/ViewController.swift
+++ DevProfile/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var devslopesProfileImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        devslopesProfileImg.layer.cornerRadius = 20
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
-
-
+    
 }
 
